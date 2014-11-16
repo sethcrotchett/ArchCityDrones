7b0cadced4664672bdf596a6c3b9ad876ec16877
Seth Crotchett <sethcrotchett@gmail.com>
2014-11-16 13:53:03 -0600

    initial check in

---
+++ ArchCityDrones/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ArchCityDrones.Startup))]
+namespace ArchCityDrones
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
